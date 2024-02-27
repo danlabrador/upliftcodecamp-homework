cffd5e9eeab36fcd8b76c2bcf4a4fb563d6da23e
Dan Labrador <thonydaniellelabrador@gmail.com>
2024-02-27 06:11:53 -0500

    perf: edit string characters directly instead of creating new strings for hideFirstName()
    hw-hide-name

--- hw-hide-name/index.js
+++ hw-hide-name/index.js
@@ -5,13 +5,20 @@
  * @returns {string} - The hidden name
  */
 const hideName = (name) => {
-  // Validate the input
-  if (!isValidInput(name)) {
+  // Validate input: The input should be a string
+  if (typeof name !== 'string') { 
+    return `Input provided is not a valid name`;
+  }
+  
+  // Validate string
+  const words = name.split(' ');
+  if (words.length < 2) { // The input should have at least 2 words
+    return `Input provided is not a valid name`;
+  } else if (name.match(/[^a-zA-Z\s]/)) { // The input should only contain letters and spaces
     return `Input provided is not a valid name`;
   }
 
   // Split the name into words
-  const words = name.split(' ');
   const numWords = words.length;
   const labeledWords = {
     firstName: words.slice(0, numWords - 1).join(' '),
@@ -28,60 +35,26 @@ const hideName = (name) => {
 
 
 /**
- * Check if the name is valid - it should be at least 2 words long and should not
- * contain any special characters or numbers
- * @param {string} name - The name to be validated
- * @returns {boolean} - True if the name is valid, otherwise false
- */
-const isValidInput = (name) => {
-  // Validate Input: Check if the name is a string
-  if (typeof name !== 'string') {
-    return false;
-  }
-
-  // Validate Input: Check if the name is at least 2 words long
-  if (name.split(' ').length < 2) {
-    return false;
-  }
-
-  // Validate Input: Check if the name has no special characters or numbers
-  if (name.match(/[^a-zA-Z\s]/)) {
-    return false;
-  }
-
-  return true;
-}
-
-
-/**
  * Hides the first name by hiding the middle characters without modifying the
  * original string
  * @param {string} firstName - The first name to be hidden
  * @returns {string} - The hidden first name
  */
 const hideFirstName = (firstName) => {
-  const firstName2DArr = firstName.split(' ').map(word => word.split(''));
-
-  firstName2DArr.map(word => {
+  return firstName.split(' ').map(word => {
     // If the word is 3 characters or less, return the word
     if (word.length <= 3) {
-      return word
+      return word;
     }
 
-    // If the word is 4 characters or more, hide the middle characters
-    const numChars = word.length;
-    const startHideIndex = 2;
-    const endHideIndex = numChars - 1;
-
-    for (let i = startHideIndex; i < endHideIndex; i++) {
-      word[i] = '*';
-    }
-
-    return word;
-  })
-
-  // Join the words and return the result in uppercase
-  return firstName2DArr.map(word => word.join('')).join(' ').toUpperCase();
+    // Start hiding the middle characters for words with 4 characters or more
+    return word.split('').map((char, index) => {
+      if (index > 1 && index < word.length - 1) {
+        return '*';
+      }
+      return char;
+    }).join('');
+  }).join(' ').toUpperCase();
 }
 
 /**
@@ -90,4 +63,6 @@ const hideFirstName = (firstName) => {
  * @param {string} lastName - The last name to be hidden
  * @returns {string} - Abbreviated last name
  */
-const hideLastName = (lastName) =>  lastName[0].toUpperCase() + `.`;          
+const hideLastName = (lastName) =>  {
+  return lastName[0].toUpperCase() + '.';
+}
