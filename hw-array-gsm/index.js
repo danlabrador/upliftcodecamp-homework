eb0feb31d82551a987ebbb46388026e6f8c31a42
Dan Labrador <thonydaniellelabrador@gmail.com>
2024-03-01 08:47:05 -0500

    style: use more descriptive variable names
    hw-array-gsm

--- hw-array-gsm/index.js
+++ hw-array-gsm/index.js
@@ -1,12 +1,12 @@
 /**
  * @func getGreatestSum() -This function takes a NxN array of numbers and returns the greatest sum of either a row or column.
- * @param {num[N][N]} arr - NxN array of numbers
+ * @param {num[N][N]} matrix - NxN array of numbers
  * @returns {number} - the greatest sum of either a row or column
  */
 
-const getGreatestSum = (arr) => {
+const getGreatestSum = (matrix) => {
   // Check if input is invalid
-  if (!checkIfIsValid(arr)) {
+  if (!checkIfIsValid(matrix)) {
     return 'Invalid input';
   }
 
@@ -14,13 +14,13 @@ const getGreatestSum = (arr) => {
   const rowSums = [];
   const colSums = [];
 
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < matrix.length; i++) {
     let rowSum = 0;
     let colSum = 0;
 
-    for (let j = 0; j < arr.length; j++) {
-      rowSum += arr[i][j];
-      colSum += arr[j][i];
+    for (let j = 0; j < matrix.length; j++) {
+      rowSum += matrix[i][j];
+      colSum += matrix[j][i];
     }
 
     rowSums.push(rowSum);
@@ -32,19 +32,19 @@ const getGreatestSum = (arr) => {
 }
 
 // Validate input
-const checkIfIsValid = (arr) => {
+const checkIfIsValid = (matrix) => {
   // Return false if input is not an array
-  if (!Array.isArray(arr)) { 
+  if (!Array.isArray(matrix)) { 
     return false;
   } 
 
   // Return false if input is not an NxN array
-  if (!arr.every(el => el.length === arr.length)) {
+  if (!matrix.every(row => row.length === matrix.length)) {
     return false;
   }
 
   // Return false if input is not an array of numbers
-  if (!arr.every(el => el.every(num => typeof num === 'number'))) {
+  if (!matrix.every(row => row.every(element => typeof element === 'number'))) {
     return false;
   }
 
