1068560c73afb518b854098b29099ed081e366c6
Dan Labrador <thonydaniellelabrador@gmail.com>
2024-03-25 06:46:33 -0400

    docs: add documentation for swapElements()

--- hw-sorting/swapElements.js
+++ hw-sorting/swapElements.js
@@ -1,3 +1,14 @@
+/**
+ * Swaps two elements in an array.
+ * 
+ * @param { string[] } strArr - array of strings
+ * @param { number } idx1 - index of the first element to swap
+ * @param { number } idx2 - index of the second element to swap
+ * 
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
 const swapElements = (strArr, idx1, idx2) => {
   const temp = strArr[idx1];
   strArr[idx1] = strArr[idx2];
